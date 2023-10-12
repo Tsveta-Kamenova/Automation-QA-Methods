80106df1f1055d4a1d77043b23a33d591c11d280
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-10-12 22:26:33 +0300

    Initial commit

---
+++ 01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace _01.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void signOfIntegerNumbers(int grade)
+            {
+
+                if (grade > 0 )
+                    Console.WriteLine("The number {0} is positive.",grade);
+                else if (grade < 0)
+                    Console.WriteLine("The number {0} is negative.", grade);
+                else
+                    Console.WriteLine("The number {0} is zero.", grade);
+            }
+
+            int grade = int.Parse(Console.ReadLine() ?? "5");
+            signOfIntegerNumbers(grade);
+        }
+    }
+}
---
+++ 07.Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
